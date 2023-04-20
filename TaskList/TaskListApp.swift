873cd22cdea3833c295651053e2c20d8757fc742
Tarikvillalobos <tarik.villalobos@gmail.com>
2023-04-20 16:03:26 -0300

    Initial Commit

---
+++ TaskList/TaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListApp.swift
+//  TaskList
+//
+//  Created by Tarik Villalobos on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
