7cc9c39dac99d977e5ee129486e6ae0ec699c800
Tarikvillalobos <tarik.villalobos@gmail.com>
2023-04-20 16:08:49 -0300

    Post

--- TaskList/ContentView.swift
+++ TaskList/ContentView.swift
@@ -1,21 +1,97 @@
-//
-//  ContentView.swift
-//  TaskList
-//
-//  Created by Tarik Villalobos on 20/04/23.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct Task: Identifiable {
+    var id = UUID()
+    var name: String
+    var isCompleted: Bool
+}
+
+class TaskList: ObservableObject {
+    @Published var tasks: [Task] = []
+    @Published var newTask = ""
+    
+    func addTask() {
+        tasks.append(Task(name: newTask, isCompleted: false))
+        newTask = ""
+    }
+    
+    func toggleTask(task: Task) {
+        if let index = tasks.firstIndex(where: { $0.id == task.id }) {
+            tasks[index].isCompleted.toggle()
         }
-        .padding()
+    }
+    
+    func removeCompletedTasks(atOffsets offsets: IndexSet) {
+        tasks.remove(atOffsets: offsets)
+    }
+}
+
+struct TaskRow: View {
+    @ObservedObject var taskList: TaskList
+    var task: Task
+    
+    var body: some View {
+        HStack {
+            Text(task.name)
+                .strikethrough(task.isCompleted)
+            Spacer()
+            if task.isCompleted {
+                Image(systemName: "trash")
+                    .foregroundColor(.red)
+                    .onTapGesture {
+                        if let index = taskList.tasks.firstIndex(where: { $0.id == task.id }) {
+                            taskList.tasks.remove(at: index)
+                        }
+                    }
+            } else {
+                Button(action: {
+                    taskList.toggleTask(task: task)
+                }) {
+                    Image(systemName: task.isCompleted ? "checkmark.square" : "square")
+                }
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var taskList = TaskList()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Nova Tarefa", text: $taskList.newTask)
+                        .padding(.horizontal)
+                    Button(action: {
+                        taskList.addTask()
+                    }) {
+                        Image(systemName: "plus")
+                    }.padding(.horizontal)
+                }
+                .frame(height: 40)
+                .background(Color.gray.opacity(0.1))
+                .cornerRadius(20)
+                .padding(.horizontal)
+                
+                List {
+                    ForEach(taskList.tasks) { task in
+                        TaskRow(taskList: taskList, task: task)
+                    }
+                    .onDelete(perform: taskList.removeCompletedTasks)
+                }
+                
+                Button(action: {
+                    taskList.removeCompletedTasks(atOffsets: IndexSet(taskList.tasks.indices.filter { taskList.tasks[$0].isCompleted }))
+                }) {
+                    Text("Excluir Tarefas Concluídas")
+                }
+                .padding(.top)
+                .foregroundColor(.red)
+                
+            }
+            .navigationBarTitle("Minhas Tarefas")
+        }
     }
 }
 
